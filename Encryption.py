1945ca262aa5c4fa6600e8ed53fd592f38656e85
Filip Garbacik <F.Garbacik.22@unimail.winchester.ac.uk>
2025-10-29 22:47:58 +0000

    Added parents

--- Encryption.py
+++ Encryption.py
@@ -1,4 +1,9 @@
 import main
 
-def init_encryption_window():
-    window = main.Window("Encryption", 300, 300)
+prime1 = 857501480975746940207863753291
+prime2 = 884671140447678060778666300543
+
+def init_encryption_window(main_window):
+    window = main.Window("Encryption", 300, 300, True)
+    window.parent = main_window
+    print(prime1)
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import tkinter.messagebox
 from tkinter import *
 from tkinter import ttk
 import functools
 
 import Encryption
 
-def button_Click(name):
-    window.withdraw()
-    Encryption.init_encryption_window()
+active_window = None
+main_window = None
 
-def is_Even(number):
-    if number % 2 == 0:
-        return 1
-    else:
-        return 0
+
+def button_click(name, parent):
+    window.withdraw()
+    match name:
+        case 'Encryption':
+            Encryption.init_encryption_window(parent)
+        case _:
+            tkinter.messagebox.showerror("Error", "Missing page for \"" + str(name) + "\" button")
+            window.deiconify()
 
 
 class Button(Tk):
@@ -21,18 +25,31 @@ class Button(Tk):
 
 
 class Window(Tk):
-    def __init__(self, title, h, w):
+    def __init__(self, title, h, w, back):
+        self.parent = None
         def cleanly_exit():
             exit(0)
+        def go_Back():
+            if self.parent:
+                self.parent.deiconify()
+            self.destroy()
+
         Tk.__init__(self)
         self.title(title)
         container = ttk.Frame(self, height=h, width=w)
-        container.grid(row=0, column=0)
+        container.grid(row=0, column=1)
         self.protocol('WM_DELETE_WINDOW', cleanly_exit)
+        global active_window
+        active_window = self
+
+        if back == True:
+            exit_Button = ttk.Button(container, text="Back", command=go_Back)
+            exit_Button.grid(row=0, column=0)
 
 
 if __name__ == "__main__":  # Need this here for the class to run
-    window = Window("Algorithm Selection", 100, 100)
+    window = Window("Algorithm Selection", 100, 100, False)
+    main_window = window
     button_Names = ["Encryption", "Fibonacci Numbers", "Sorting", "Brute Force",
                     "Randomised", "Recursion", "Search", "Dynamic Programming",
                     "Behavioral Design Pattern", "Creational Design Pattern",
@@ -42,8 +59,6 @@ if __name__ == "__main__":  # Need this here for the class to run
     button_Flag = 0
     for i in range(button_Range):
         counter = counter + 1
-        print("i = " + str(i))
-        print("counter = " + str(counter))
         if i > button_Range/2:
             button_Flag = 1
             if counter == i:
@@ -51,7 +66,7 @@ if __name__ == "__main__":  # Need this here for the class to run
         else:
             button_Flag = 0
         button = ttk.Button(window, text=str(button_Names[i]), # Planned: make graphical buttons using classes
-            command=functools.partial(button_Click, button_Names[i]))
+            command=functools.partial(button_click, button_Names[i], main_window))
         button.grid(column=counter, row=button_Flag)
 
     window.mainloop()
