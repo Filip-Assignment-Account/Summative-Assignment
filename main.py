34707416d5e553588fe881629f3f21a4c2709311
Filip Garbacik <F.Garbacik.22@unimail.winchester.ac.uk>
2025-10-29 17:02:35 +0000

    buttons work now

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ from tkinter import *
 from tkinter import ttk
 
 
+def is_Even(number):
+    if number % 2 == 0:
+        return 1
+    else:
+        return 0
+
+
 class Window(Tk):
     def __init__(self, title):
         Tk.__init__(self)
         self.title(title)
-        container = ttk.Frame(self, height=300, width=300)
+        container = ttk.Frame(self, height=3, width=3)
         container.grid(row=0, column=0)
-        self.mainloop()
 
 
-if __name__ == "__main__": # Need this here for the class to run
+if __name__ == "__main__":  # Need this here for the class to run
     window = Window("egg")
-    Button_names = {"Encryption", "Fibonacci Numbers", "Sorting", "Brute Force",
+    button_Names = ["Encryption", "Fibonacci Numbers", "Sorting", "Brute Force",
                     "Randomised", "Recursion", "Search", "Dynamic Programming",
                     "Behavioral Design Pattern", "Creational Design Pattern",
-                    "Structural Design Pattern"}
+                    "Structural Design Pattern"]
+    for i in range(len(button_Names)):
+        button = ttk.Button(window, text=str(button_Names[i]))
+        button.grid(column=i, row=is_Even(i))
 
     window.mainloop()
-
-
-
-
-
