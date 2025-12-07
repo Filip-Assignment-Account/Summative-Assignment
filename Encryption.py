1785ef2302e6912d2e8edbecb1ff02dcb6f0c1ef
Filip Garbacik <F.Garbacik.22@unimail.winchester.ac.uk>
2025-12-07 17:11:41 +0000

    Partially implemented the rsa encryption algorithm

--- Encryption.py
+++ Encryption.py
@@ -1,20 +1,181 @@
+import tkinter.messagebox
 import main
-
-prime1 = 857501480975746940207863753291
-prime2 = 884671140447678060778666300543
+import math
+import functools
+from tkinter import *
+from tkinter import ttk
+prime1 = 52609494872530492621
+prime2 = 52609494872530492621
 
 class Encryption:
-    def Decrypt(self):
-        print("Decrypt")
-    def Encrypt(self):
-        print("Encrypted")
+    def generate_key(self, key: tuple | None):
+        print("generate_key: key value: " + str(key))
+        if key:
+            try:
+                print("generate_key: key value received: " + str(key))
+                self.prime1 = int(key[0])
+                self.prime2 = int(key[1])
+            except Exception as e:
+                print("Exception in Generate key: " + str(e))
+                tkinter.messagebox.showerror("Error", "Exception thrown: " + str(e))
+                return
+        else:
+            self.prime1 = int(prime1)
+            self.prime2 = int(prime2)
 
-    def __init__(self):
-        print("placeholder")
+        print("generate_key: Primes set: " + str(self.prime1) + " and " + str(self.prime2))
+
+        self.primemult = self.prime1 * self.prime2
+        primemult_minusone = (self.prime1-1)*(self.prime2-1)
+        exponent = 2
+
+        while(exponent<primemult_minusone):
+            if (math.gcd(exponent, primemult_minusone) == 1):
+                break
+            else:
+                exponent += 1
+                print("generate_key_loop: " + str(exponent))
+
+        k = 2
+        mult_priv = ((k*primemult_minusone)+1)/exponent
+        private_key = {mult_priv, self.primemult}
+        public_key = {exponent, self.primemult}
+
+        print("generate_key: Key pair generated: " + str(private_key) + str(public_key))
+        return private_key, public_key
+
+    def encrypt_text(self, msg: str, key: tuple | None) -> str:
+        print("encrypt: key value: " + str(key))
+        keys = self.generate_key(key)
+        print("encrypt: got key pair: " + str(keys))
+
+        public_key = keys[1]
+        n, e = public_key
+
+        encrypted_msg = ''
+        try:
+            for letter in msg:
+                mangled_letter = (ord(letter) ** e) % n
+                print("encrypt loop: " + str(letter))
+                print("encrypt loop: " + str(e))
+                print("encrypt loop: " + str(mangled_letter))
+                while mangled_letter > 1114111: # chr has a limit of 1114111, this ensures that we're under that limit
+                    mangled_letter = mangled_letter // 2
+                    print("mangled_letter loop: " + str(mangled_letter))
+
+                encrypted_msg = encrypted_msg + chr(mangled_letter)
+                print("encrypt loop: " + encrypted_msg)
+        except ValueError as e:
+            tkinter.messagebox.showerror("Prime too big", "Inputted prime number was invalid or too big, error was: " + str(e))
+        print(encrypted_msg)
+        print("Encrypted")
+        return encrypted_msg
+
+    def Decrypt(self, msg: str, key: tuple | None) -> str:
+        keys = self.generate_key(key)
+
+        private_key = keys[0]
+        n, d = private_key
+
+        decrypted_msg = ''
+        try:
+            for letter in msg:
+                mangled_letter = (ord(letter) ** d) % n
+                print("decrypt loop: " + str(letter))
+                print("decrypt loop: " + str(d))
+                print("decrypt loop: " + str(mangled_letter))
+
+                while mangled_letter > 11141111:
+                    mangled_letter = mangled_letter // 2
+                    print("mangled_letter loop: " + str(mangled_letter))
+
+                decrypted_msg = decrypted_msg + chr(mangled_letter)
+                print("decrypt loop: " + decrypted_msg)
+        except ValueError as e:
+            tkinter.messagebox.showerror("Prime too big", "Inputted prime number was invalid or too big, error was: " + str(e))
+
+        print(private_key)
+        print("Decrypt")
+        print(decrypted_msg)
+        return decrypted_msg
+
+    def __init__(self, window):
+        def is_prime(number):
+            print("isprime: Checking if "+ str(number) + " is prime")
+            if number <= 1:
+                return False
+            for i in range(2, int(number**0.5) + 1):
+                print("isprime: i is currently: "+ str(i))
+                print(number % 1)
+                if number % i == 0:
+                    return False
+            return True
+
+        def button_encrypt(is_decrypt: bool) -> None:
+            prime1_text = entry_prime1.get()
+            prime2_text = entry_prime2.get()
+            print("button_encrypt: value of prime1_text: " + str(prime1_text))
+            print("button_encrypt: value of prime2_text: " + str(prime2_text))
+
+            if prime1_text == '' and prime2_text == '' and not is_decrypt:
+                self.encrypt_text(entry_msg.get('1.0', 'end-1c'), None) # Gets text from beginning until the end of the text box
+                return
+            elif prime1_text == '' and prime2_text == '' and is_decrypt:
+                self.Decrypt(entry_msg.get('1.0', 'end-1c'), None)
+
+            else:
+                try:
+                    print("Trying to run isprime")
+                    if is_prime(int(prime1_text)) and is_prime(int(prime2_text)) and not is_decrypt:
+                        self.encrypt_text(entry_msg.get('1.0', 'end-1c'), (prime1_text, prime2_text))
+                    elif is_prime(int(prime1_text)) and is_prime(int(prime2_text)) and not is_decrypt:
+                        self.Decrypt(entry_msg.get('1.0', 'end-1c'), (prime1_text, prime2_text))
+                    else:
+                        tkinter.messagebox.showerror("Error", "Value must be a prime integer")
+                except Exception as e:
+                    print(e)
+                    tkinter.messagebox.showerror("Error", "Value must be a prime integer")
+
+
+        def validate_input(input):
+            if input.isdigit() or input == "":
+                print("vaildation successful")
+                return True
+            else:
+                tkinter.messagebox.showerror("Validation failed")
+                return False
+
+        window.add_title("Pick two prime numbers (optional): ", 1, 1)
+
+        entry_container = ttk.Frame(window, height=10, width=100)
+        entry_container.grid(row=2, column=1)
+
+        vcmd = window.register(validate_input)
+
+        entry_prime1 = Entry(entry_container, validate="key", validatecommand=(vcmd, "%P"))
+        entry_prime1.grid(row=2, column=1, pady=10)
+        entry_prime2 = Entry(entry_container)
+        entry_prime2.grid(row=2, column=2, padx=10)
+
+        msg_label = ttk.Label(window, text="Enter a message to encrypt/decrypt")
+        msg_label.grid(row=3, column=1, pady=(20,0))
+
+        msg_container = ttk.Frame(window, height=10, width=100)
+        msg_container.grid(row=4, column=1)
+
+        entry_msg = Text(msg_container, width=30, height=2)
+        entry_msg.grid(row=0, column=0, padx=(30,0))
+
+        encrypt_button = Button(window, text="Encrypt text", command=functools.partial(button_encrypt, False))
+        encrypt_button.grid(row=4, column=3)
+
+        decrypt_button = Button(window, text="Decrypt text", command=functools.partial(button_encrypt, True))
+        decrypt_button.grid(row=5, column=3)
+        print(prime1)
 
 
 def init_encryption_window(main_window):
-    window = main.Window("Encryption", 300, 300)
-    window.Add_Back_Button()
+    window = main.Window("Encryption", 40, 400)
+    window.add_back_button()
     window.parent = main_window
-    print(prime1)
+    encryption = Encryption(window)
--- main.py
+++ main.py
@@ -9,36 +9,72 @@ active_window = None
 main_window = None
 
 
-def button_click(name, parent):
+def button_click(name: str, parent: str) -> None:
+    """
+    Handles button clicks and runs corresponding window functions from other windows
+    :param name: Name of the window
+    :param parent: Parent of the window (usually this one, main.py)
+    :return: None
+    """
     window.withdraw()
     match name:
         case 'Encryption':
             Encryption.init_encryption_window(parent)
+        case 'Fibonacci Numbers':
+            print("fib")
         case _:
             tkinter.messagebox.showerror("Error", "Missing page for \"" + str(name) + "\" button")
             window.deiconify()
 
 
-class Button(Tk):
-    def __init__(self):
-        #super().__init__() # May be needed, find out later
-        print()
-
-
 class Window(Tk):
-    def go_back(self):
+    def go_back(self) -> None:
+        """
+        Destroys current window and goes back to previous window
+        :return: None.
+        """
         if self.parent:
             self.parent.deiconify()
         self.destroy()
 
-    def Add_Back_Button(self):
+    def add_back_button(self) -> None:
+        """
+        Adds back button to the top left of a window.
+        :return: None.
+        """
         exit_button = ttk.Button(self.container, text="Back", command=self.go_back)
-        exit_button.grid(row=0, column=0)
+        exit_button.place(x=0, y=0)
 
-    def __init__(self, title, h, w):
+    def add_title(self, text: str, gridx: int, gridy: int) -> None:
+        """
+        Adds title to a window.
+        Args:
+
+        :param text: Text to add as title.
+        :param gridx: Same as column=
+        :param gridy: Same as row=
+        :return: None.
+        """
+        title_container = ttk.Frame(self, height=1, width=50)
+        title_container.grid(row=gridy, column=gridx)
+        title_label = ttk.Label(title_container, text=text)
+        title_label.config(font=("Arial", 15))
+        title_label.grid(row=0, column=0, padx=(40, 0))
+
+    def __init__(self, title: str, h: int, w: int) -> None:
+        """
+        Creates a window with a title, height and weight. Automatically creates a container inside the window and handles exiting behaviour.
+        :param title: Title of the window
+        :param h: Height of the container within the window
+        :param w: Width of the container in the window
+        """
         self.parent = None
 
-        def cleanly_exit():
+        def cleanly_exit() -> None:
+            """
+            Exits application when no windows are open so that the application doesn't remain dormant in the background
+            :return: None
+            """
             exit(0)
 
         Tk.__init__(self)
@@ -48,6 +84,14 @@ class Window(Tk):
         self.protocol('WM_DELETE_WINDOW', cleanly_exit)
         global active_window
         active_window = self
+
+
+class Button(Tk):
+    def __init__(self, window: Window, function: any) -> None:
+        #super().__init__() # May be needed, find out later
+        self.Button = ttk.Button(window, command=functools.partial(function, button_Names[i], window))
+    def Grid_button(self, column, row):
+        self.Button.grid(column=column, row=row)
 
 
 if __name__ == "__main__":  # Need this here for the class to run
