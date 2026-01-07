e34206e152ce22f850ad713138004d656710cfba
Filip Garbacik <F.Garbacik.22@unimail.winchester.ac.uk>
2026-01-07 23:31:14 +0000

    testing: add initial testing for encryption

---
+++ tests/encryption_test.py
@@ -0,0 +1,11 @@
+import pytest
+import src.summative_project.Encryption as Encryption
+import src.summative_project.main as main
+
+def test_encryption():
+    encryption = Encryption()
+    encrypted_text = encryption.encrypt_text("test", None)
+    unencrypted_text = encryption.decrypt_text(encrypted_text, None)
+    assert unencrypted_text == encrypted_text
+
+
