909bafff9e6ec5d6c7441b0dd59c78b25dec5a9a
Filip Garbacik <F.Garbacik.22@unimail.winchester.ac.uk>
2026-01-14 13:48:39 +0000

    adding more tests and repurposing this file as a general unit tests file

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,14 +1,21 @@
 import sys
 sys.path.append("..\..\src\summative_project")
-import pytest
 import src.summative_project.Encryption as Encryption
-import src.summative_project.main as main
+import src.summative_project.Cards as Cards
 
+"""Functions run from here cannot find main, for example running encryption.encrypt_text() will complain that
+encryption cannot find main , no idea why"""
 
 def test_encryption():
     encryption = Encryption()
     encrypted_text = encryption.encrypt_text("test", None)
     unencrypted_text = encryption.decrypt_text(encrypted_text, None)
     assert unencrypted_text == encrypted_text
+
+def test_cards():
+    cards_object = Cards()
+    cards = cards.get_full_deck()
+    assert len(cards) == 52
+
 
 
